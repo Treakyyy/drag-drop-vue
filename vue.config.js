50788e940c33a2d8941dad7c9d5b5b18c526b9ef
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-10-11 01:11:43 +0300

    edits

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/drag-drop-vue/' // Замените 'your-repo-name' на имя вашего репозитория
+  ? '/drag-drop-vue/'
   : '/'
 
 })
